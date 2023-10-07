c8079cd8812cfd79495343fb4c44cb838e226966
funny-family <maksvadik228@gmail.com>
2023-10-07 23:51:18 +0700

    update types

--- lib/tooltip/tooltip.types.ts
+++ lib/tooltip/tooltip.types.ts
@@ -1,7 +1,31 @@
-export type TooltipDirective = <
-  TElement extends HTMLElement,
-  TValue extends any
->(
-  el: TElement,
-  value: TValue
+import { type JSX } from 'solid-js';
+import type { RequireAtLeastOne } from '../types';
+
+// type TooltipAccessor<TElement extends HTMLElement> = () => {
+//   element: TElement;
+// };
+
+export type TooltipDirective = (
+  element: HTMLElement,
+  accessor: () => {
+    element: HTMLElement;
+    option?: {
+      optionString?: string;
+      optionNumber?: number;
+      optionFunction?: Function;
+    };
+  }
 ) => void;
+
+// type TooltipElement = JSX.Element | (() => JSX.Element);
+
+export type TooltipDirectiveDeclaration = {
+  tooltip: {
+    element: JSX.Element | (() => JSX.Element);
+    option?: RequireAtLeastOne<{
+      optionString?: string;
+      optionNumber?: number;
+      optionFunction?: Function;
+    }>;
+  };
+};
