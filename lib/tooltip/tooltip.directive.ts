aab3ef05fccdc21d62517dcb77416766158a1ba9
funny-family <maksvadik228@gmail.com>
2023-10-23 23:34:25 +0700

    update logic of directive

--- lib/tooltip/tooltip.directive.ts
+++ lib/tooltip/tooltip.directive.ts
@@ -1,81 +1,120 @@
 import { onCleanup } from 'solid-js';
-import { type TooltipDirective } from './tooltip.types';
+import type {
+  TooltipDirectiveOption,
+  TooltipDirective,
+  TooltipDirectiveAccessor,
+} from './tooltip.types';
 
-const tooltipContainer = document.createElement('div');
-// tooltipContainer.setAttribute('id', 'solid-js-tooltips');
-tooltipContainer.classList.add('solid-js-tooltips');
-document.body.appendChild(tooltipContainer);
+const tooltipsContainer = document.createElement('div');
+tooltipsContainer.classList.add('solid-js-tooltips');
+document.body.appendChild(tooltipsContainer);
 
-const debounce = <TCallback extends (...args: any[]) => any>(
-  callback: TCallback,
-  ms: number
-) => {
-  let timerID: number | null = null;
-
-  return function (this: any, ...args: any[]) {
-    if (timerID == null) {
-      callback.apply(this, args);
-    }
-
-    window.clearTimeout(timerID as number);
-    timerID = window.setTimeout(() => callback.apply(this, args), ms);
-    // return timerID;
-  };
-};
+var tooltipSharedZIndex = 1;
 
 export const tooltip: TooltipDirective = (element, accessor) => {
-  const accessorValue = accessor();
+  const accessorValue = accessor() as Required<
+    ReturnType<TooltipDirectiveAccessor>
+  >;
 
-  console.group();
-  console.log('arguments:', { element, accessor });
-  console.log('v:', { element, accessor, accessorValue });
-  console.groupEnd();
+  const _keepVisibleWhenHover = accessorValue?.option?.keepVisibleWhenHover;
+  const _displayOnMouseenter = accessorValue?.option?.displayOnMouseenter;
+  const _displayOnFocus = accessorValue?.option?.displayOnFocus;
+  const _position = accessorValue?.option?.position;
+  const _cacheRender = accessorValue?.option?.cacheRender;
+  const _onMouseenter = accessorValue?.option?.onMouseenter;
+  const _onMouseleave = accessorValue?.option?.onMouseleave;
+
+  (accessorValue.option as TooltipDirectiveOption) = {
+    keepVisibleWhenHover:
+      _keepVisibleWhenHover == null ? true : _keepVisibleWhenHover,
+    displayOnMouseenter:
+      _displayOnMouseenter == null ? true : _displayOnMouseenter,
+    displayOnFocus: _displayOnFocus == null ? true : _displayOnFocus,
+    cacheRender: _cacheRender == null ? false : _cacheRender,
+    position: _position == null ? 'top-center' : _position,
+    onMouseenter: _onMouseenter == null ? null : _onMouseenter,
+    onMouseleave: _onMouseleave == null ? null : _onMouseenter,
+  } as TooltipDirectiveOption;
 
   accessorValue.element.classList.add('solid-js-tooltip');
+  accessorValue.element.setAttribute('role', 'tooltip');
+  accessorValue.element.setAttribute('aria-labelledby', 'tooltip');
+  accessorValue.element.setAttribute('inert', '');
+  accessorValue.element.setAttribute('tabindex', '-1');
+  accessorValue.element.style.zIndex = `${tooltipSharedZIndex}`;
+  tooltipSharedZIndex += 1;
+
+  // const getElementBoundingClientRect = <TElement extends HTMLElement>(
+  //   element: TElement
+  // ) => {
+  //   return {
+  //     top: element.offsetTop + element.scrollTop + element.clientTop,
+  //     left: element.offsetLeft + element.scrollLeft + element.clientLeft,
+  //   };
+  // };
+
+  // let parentRect: DOMRect;
+  let parentStyle: CSSStyleDeclaration;
 
   const onMouseenter = function (this: HTMLElement, event: MouseEvent) {
-    console.log('onMouseenter:', event);
+    const parentRect = element.getBoundingClientRect();
+    parentStyle = getComputedStyle(element);
 
-    const parentElRect = element.getBoundingClientRect();
-    const parentElComputedStyle = getComputedStyle(element);
+    // const tooltipRect = accessorValue.element.getBoundingClientRect();
 
-    accessorValue.element.style.setProperty(
-      '--tooltip-parent-position-y',
-      `${parentElRect.top}px`
-    );
-    accessorValue.element.style.setProperty(
-      '--tooltip-parent-position-x',
-      `${parentElRect.left}px`
-    );
+    console.log({
+      // tooltipRect,
+      parentRect,
+      that: this,
+      event,
+      e: this.getBoundingClientRect(),
+      tltsize: {
+        width: accessorValue.element.offsetWidth,
+        height: accessorValue.element.offsetHeight,
+      },
+    });
 
-    accessorValue.element.style.setProperty(
-      '--tooltip-parent-width',
-      parentElComputedStyle.width
-    );
-    accessorValue.element.style.setProperty(
-      '--tooltip-parent-height',
-      parentElComputedStyle.height
-    );
+    // console.group();
+    // console.log('arguments:', { element, accessor });
+    // console.log('v:', {
+    //   element,
+    //   accessor,
+    //   accessorValue,
+    //   tooltipElement: accessorValue.element,
+    //   parentStyle,
+    //   tooltipStyle,
+    //   parentRect,
+    // });
+    // console.groupEnd();
 
-    accessorValue.element.style.setProperty(
-      '--tooltip-parent-margin-top',
-      parentElComputedStyle.marginTop
-    );
-    accessorValue.element.style.setProperty(
-      '--tooltip-parent-margin-right',
-      parentElComputedStyle.marginRight
-    );
-    accessorValue.element.style.setProperty(
-      '--tooltip-parent-margin-bottom',
-      parentElComputedStyle.marginBottom
-    );
-    accessorValue.element.style.setProperty(
-      '--tooltip-parent-margin-left',
-      parentElComputedStyle.marginLeft
-    );
+    console.log(parentRect);
 
-    // element.parentElement?.appendChild(accessorValue.element);
-    tooltipContainer?.appendChild(accessorValue.element);
+    if (accessorValue.option.position === 'top-center') {
+      accessorValue.element.style.top = `${parentRect.top}px`;
+      accessorValue.element.style.left = `${parentRect.left}px`;
+      accessorValue.element.style.transform = `translate(calc((${parentRect.width}px / 2) - 50%), 0)`;
+    }
+
+    if (accessorValue.option.position === 'right-center') {
+      //
+    }
+
+    if (accessorValue.option.position === 'bottom-center') {
+      accessorValue.element.style.top = `${parentRect.top}px`;
+      accessorValue.element.style.left = `${parentRect.left}px`;
+      accessorValue.element.style.transform = `translate(calc((${parentRect.width}px / 2) - 50%), calc(${parentRect.height}px + 100%))`;
+    }
+
+    if (accessorValue.option.position === 'left-center') {
+      accessorValue.element.style.top = `${parentRect.top}px`;
+      accessorValue.element.style.left = `${
+        parentRect.left + parentRect.width
+      }px`;
+      accessorValue.element.style.transform = `translate(0, calc(${parentRect.width}px / 2))`;
+      // accessorValue.element.style.transform = `translate(calc((${parentRect.width}px / 2) - 50%), ${parentRect.height}px)`;
+    }
+
+    tooltipsContainer?.appendChild(accessorValue.element);
 
     if (accessorValue.option?.onMouseenter != null) {
       accessorValue.option.onMouseenter.call(this, event);
@@ -83,9 +122,10 @@ export const tooltip: TooltipDirective = (element, accessor) => {
   };
 
   const onMouseleave = function (this: HTMLElement, event: MouseEvent) {
-    console.log('onMouseleave:', event);
+    // console.log({ that: this, event });
+    // console.log('onMouseleave:', event);
 
-    tooltipContainer?.removeChild(accessorValue.element);
+    // tooltipsContainer?.removeChild(tooltipWrapper);
 
     if (accessorValue.option?.onMouseleave != null) {
       accessorValue.option.onMouseleave.call(this, event);
