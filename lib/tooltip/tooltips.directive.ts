bbc83eb8192946608b1705e2d997157a34cf1bf3
funny-family <maksvadik228@gmail.com>
2023-12-05 11:27:53 +0700

    fix "bottom-left" position

--- lib/tooltip/tooltips.directive.ts
+++ lib/tooltip/tooltips.directive.ts
@@ -266,11 +266,11 @@ export var tooltips1 = ((element, accessor) => {
       }
 
       if (option.position === 'bottom-left') {
-        tooltip.style.translate = `calc(var(${tooltipMarginY_CssVar})) calc(var(${tooltipableHeight_CssVar}) - var(${tooltipMarginY_CssVar}))`;
+        tooltip.style.translate = `calc(var(${tooltipMarginX_CssVar})) calc(var(${tooltipableHeight_CssVar}) - var(${tooltipMarginY_CssVar}))`;
       }
 
       if (option.position === 'bottom-left-corner') {
-        // tooltip.style.translate = `calc(var(${tooltipableWidth_CssVar}) - var(${tooltipMarginX_CssVar})) calc(var(${tooltipableHeight_CssVar}) - var(${tooltipMarginY_CssVar}))`;
+        // tooltip.style.translate = `calc(-100% - var(--tooltip-margin-x)) calc(var(--tooltipable-height) - var(--tooltip-margin-y))`;
       }
 
       if (option.position === 'left-bottom') {
