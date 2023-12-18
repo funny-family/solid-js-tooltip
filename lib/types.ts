83a888aa4e22c63e767320f10694417d6f9b44a9
funny-family <maksvadik228@gmail.com>
2023-12-18 19:42:56 +0700

    update shared types

--- lib/types.ts
+++ lib/types.ts
@@ -2,4 +2,10 @@ export type DeepRequired<T> = {
   [K in keyof T]: Required<DeepRequired<T[K]>>;
 };
 
-export  type NonEmpty<T> = T extends Array<infer U> ? U[] & { '0': U } : never;
+export type NonEmpty<T> = T extends Array<infer U> ? U[] & { '0': U } : never;
+
+type DataAttributeKey = `data-${string}`;
+
+export type DataAttribute = {
+  [key: DataAttributeKey]: any;
+};
