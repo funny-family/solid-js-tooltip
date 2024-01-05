658b41e8a46ce39c2d6f86cf9797eed7b8d12683
funny-family <maksvadik228@gmail.com>
2024-01-05 23:19:30 +0700

    update "tooltip" component

--- lib/tooltip/component/tooltip.component.tsx
+++ lib/tooltip/component/tooltip.component.tsx
@@ -12,8 +12,9 @@ export const Tooltip: TooltipComponent = (attrs) => {
       }
       class={`${attrs?.class || ''} solid-js-tooltip`}
       role={attrs?.role || labelName}
+      tabindex={attrs?.tabindex || -1}
+      aria-label={attrs?.['aria-label'] || labelName}
       aria-labelledby={attrs?.['aria-labelledby'] || labelName}
-      tabIndex={attrs?.tabIndex || -1}
       aria-hidden={attrs?.['aria-hidden'] == null ? true : attrs['aria-hidden']}
       inert={attrs?.inert == null ? true : attrs.inert}
     />
